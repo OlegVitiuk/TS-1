9585e299a1632593940385d293973108b5035913
oleg_valerievich <vitiukoleh@gmail.com>
2019-07-16 13:17:07 +0300

    add scss modules

--- src/front/components/App/index.tsx
+++ src/front/components/App/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import CharacterList from "../СharacterList";
-import "./App.css";
+import styles from "./app.module.scss";
 
 const App: React.SFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">header app</header>
+    <div className={styles.App}>
+      <header className={styles.App_header}>header app</header>
       <CharacterList />
     </div>
   );
