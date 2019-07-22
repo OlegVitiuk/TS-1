be66caa993df9dfbf6844ec80b96600594e8c530
oleg_valerievich <vitiukoleh@gmail.com>
2019-07-22 14:46:38 +0300

    delete pay route

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,11 @@
+import Navigation from "components/Navigation";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "routes/Home";
-import Navigation from "components/Navigation";
-import Pay from "routes/Pay";
+import ProtectedRoute from "routes/ProtectedRoute";
 import SignIn from "routes/SignIn";
 import SignUp from "routes/SignUp";
 import styles from "./app.module.scss";
-import ProtectedRoute from "routes/ProtectedRoute";
 
 const App: React.SFC = () => {
   return (
@@ -16,7 +15,6 @@ const App: React.SFC = () => {
         <ProtectedRoute path="/" component={Home} exact={true} />
         <Route exact={true} path="/sign-in" component={SignIn} />
         <Route exact={true} path="/sign-up" component={SignUp} />
-        <ProtectedRoute path="/pay" component={Pay} />
       </Router>
     </div>
   );
